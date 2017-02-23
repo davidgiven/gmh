29417b242f1c3a454177b13ff51f5fa8e8c4303b
dg@cowlark.com <dg@cowlark.com>
2017-02-23 23:24:45 +0000

    Actually start fetching message data from Gmail.

--- src/main/kotlin/com/cowlark/gmh/Database.kt
+++ src/main/kotlin/com/cowlark/gmh/Database.kt
@@ -1,5 +1,5 @@
 /* GMH
- * © 2013 David Given
+ * © 2017 David Given
  * This file is redistributable under the terms of the
  * New BSD License. Please see the COPYING file in the
  * project root for the full text.
@@ -11,34 +11,56 @@ import org.sqlite.JDBC
 import org.sqlite.SQLiteConfig
 import java.sql.Connection
 
-class Dummy {}
-
-fun connect_to_database(): Connection
-{
+fun connect_to_database(filename: String): Connection {
     val config = SQLiteConfig()
     config.enforceForeignKeys(true)
     config.setSynchronous(SQLiteConfig.SynchronousMode.OFF)
 
-    val connection = JDBC.createConnection(JDBC.PREFIX + "gmh.sqlite", config.toProperties())
+    val connection = JDBC.createConnection(JDBC.PREFIX + filename, config.toProperties())
     connection.autoCommit = false
 
     val statement = connection.createStatement()
     statement.execute("""
-      CREATE TABLE IF NOT EXISTS variables (
-        name TEXT PRIMARY KEY,
-        value TEXT
-      )
+        CREATE TABLE IF NOT EXISTS variables (
+            name TEXT PRIMARY KEY,
+            value TEXT
+        )
     """)
     statement.execute("""
-      CREATE TABLE IF NOT EXISTS messages (
-        id TEXT PRIMARY KEY,
-        received INTEGER,
-        subject TEXT,
-        value TEXT
-      )
+        CREATE TABLE IF NOT EXISTS bodies (
+            id TEXT PRIMARY KEY,
+            received INTEGER,
+            subject TEXT,
+            value TEXT
+        )
+    """)
+    statement.execute("""
+        CREATE TABLE IF NOT EXISTS summary (
+            id TEXT PRIMARY KEY
+        )
     """)
     connection.commit()
 
     return connection
 }
 
+class Database constructor(filename: String) {
+    val connection = connect_to_database(filename)
+
+    val getVarStatement = connection.prepareStatement(
+            "SELECT value FROM variables WHERE (name = ?)"
+    )
+
+    fun get(name: String, default: String): String {
+        getVarStatement.setString(1, name)
+        val results = getVarStatement.executeQuery()
+        try {
+            if (!results.next())
+                return default
+            return results.getString(1)
+        }
+        finally {
+            results.close()
+        }
+    }
+}
--- src/main/kotlin/com/cowlark/gmh/Main.kt
+++ src/main/kotlin/com/cowlark/gmh/Main.kt
@@ -9,28 +9,29 @@ package com.cowlark.gmh
 
 import com.sun.mail.gimap.GmailFolder
 import com.sun.mail.gimap.GmailStore
+import com.sun.mail.iap.Argument
+import com.sun.mail.imap.IMAPFolder
+import javax.mail.Folder
 import javax.mail.Session
 
-fun fatal(message: String)
-{
+
+fun fatal(message: String) {
     System.err.printf("error: %s\n", message)
     System.exit(1)
 }
 
-fun log(message: String)
-{
+fun log(message: String) {
     System.err.printf("log: %s\n", message)
 }
 
-fun connect_to_imap(): GmailFolder
-{
+fun connect_to_imap(username: String, password: String): GmailFolder {
     val properties = System.getProperties()
     properties.setProperty("mail.store.protocol", "gimap")
 
     val session = Session.getDefaultInstance(properties, null)
 
     val store = session.getStore("gimap") as GmailStore
-    store.connect("imap.gmail.com", "david.given@gmail.com", "fjsqqmpawuvmrxmd")
+    store.connect("imap.gmail.com", username, password)
     if (!store.hasCapability("CONDSTORE") || !store.hasCapability("X-GM-EXT-1"))
         fatal("this doesn't look like a Gmail server")
 
@@ -38,11 +39,37 @@ fun connect_to_imap(): GmailFolder
     return folder
 }
 
-fun main(args: Array<String>)
-{
+fun main(args: Array<String>) {
     log("connecting")
-    val db = connect_to_database()
-    val folder = connect_to_imap()
+    val db = Database("gmh.sqlite")
+    val folder = connect_to_imap("david.given@gmail.com", "fjsqqmpawuvmrxmd")
+    folder.open(Folder.READ_ONLY)
 
-    log("highestModSeq = " + folder.highestModSeq)
+    val currentModSeq = db.get("modseq", "0").toLong()
+
+    if (currentModSeq == 0L) {
+        log("fetching initial message summaries (may be some time)")
+        folder.doCommand {
+            p ->
+            p.command("FETCH",
+                    Argument()
+                            .writeString("1:10")
+                            .writeArgument(
+                                Argument()
+                                        .writeString("X-GM-MSGID")
+                                        .writeString("FLAGS")
+                            )
+            ).map {
+                log(it.toString())
+            }
+        }
+    }
+    else {
+        log("fetching message summaries")
+        val result = folder.doCommand(
+                IMAPFolder.ProtocolCommand {
+                    p ->
+                    p.simpleCommand("SEARCH", null)
+                })
+    }
 }
