a99f286be3ce2d0617cee0ac19942d82d5420da5
dg@cowlark.com <dg@cowlark.com>
2017-02-28 22:58:37 +0000

    Progress bar now autosizes based on screen width.

--- src/main/kotlin/com/cowlark/gmh/lib/Progressbar.kt
+++ src/main/kotlin/com/cowlark/gmh/lib/Progressbar.kt
@@ -10,7 +10,6 @@ package com.cowlark.gmh.lib
 import java.io.Closeable
 
 private val PROGRESS_BAR_CHARS = arrayOf(" ", "▏","▎","▍","▌","▋","▊","▉","█")
-private val PROGRESS_BAR_LEN = 43
 private val WIDTH = (System.getenv("COLUMNS") ?: "80").toInt()
 
 class Progressbar(val message: String, val max: Int) : Closeable {
@@ -33,9 +32,21 @@ class Progressbar(val message: String, val max: Int) : Closeable {
     }
   }
 
+  private fun statusString(now: Long, progress: Int): String {
+    val sb = StringBuilder()
+    val persec = progress.toDouble () / (now - startTime).toDouble() * 1000.0
+
+    val timeSoFar = (now - startTime).toDouble() / 1000.0
+    val estimatedTotal = max.toDouble() / persec
+    sb.append(" $progress/$max; " + persec.toInt() + "/sec, " + asTime(estimatedTotal - timeSoFar) + " left")
+    return sb.toString()
+  }
+
   fun show(progress: Int) {
     val now = System.currentTimeMillis()
-    if ((now - lastUpdate) > 250) {
+    if ((now - lastUpdate) > 200) {
+      val status = statusString(now, progress)
+      val width = WIDTH - status.length
       val sb = StringBuilder()
 
       sb.append("\r")
@@ -43,12 +54,12 @@ class Progressbar(val message: String, val max: Int) : Closeable {
       sb.append("\r")
 
       if (progress == max)
-        sb.append(PROGRESS_BAR_CHARS[PROGRESS_BAR_CHARS.size - 1].repeat(PROGRESS_BAR_LEN))
+        sb.append(PROGRESS_BAR_CHARS[PROGRESS_BAR_CHARS.size - 1].repeat(width))
       else {
-        val scaledProgress = (progress * PROGRESS_BAR_LEN * PROGRESS_BAR_CHARS.size) / max
+        val scaledProgress = (progress * width * PROGRESS_BAR_CHARS.size) / max
         val numfilled = scaledProgress / PROGRESS_BAR_CHARS.size
         val subchar = scaledProgress % PROGRESS_BAR_CHARS.size
-        var numempty = PROGRESS_BAR_LEN - numfilled - 1
+        var numempty = width - numfilled - 1
 
         sb.append(PROGRESS_BAR_CHARS[PROGRESS_BAR_CHARS.size - 1].repeat(numfilled))
         if (subchar >= 0)
@@ -56,12 +67,8 @@ class Progressbar(val message: String, val max: Int) : Closeable {
         sb.append(PROGRESS_BAR_CHARS[0].repeat(numempty))
       }
 
-      val persec = progress.toDouble () / (now - startTime).toDouble() * 1000.0
-      val timeSoFar = (now - startTime).toDouble() / 1000.0
-      val estimatedTotal = max.toDouble() / persec
-
-      sb.append(" $progress/$max; " + persec.toInt() + "/sec, " + asTime(estimatedTotal - timeSoFar) + " left")
-      System.err.print(sb.toString())
+      sb.append(status)
+      System.err.print(sb)
       System.err.flush()
       lastUpdate = now
     }
